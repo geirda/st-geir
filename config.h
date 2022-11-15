49a15e7442bf4cc7805f4a4a5b5adc3903ebd974
Geir Damstuen <geir@gmail.com>
2022-11-15 23:21:37 +0100

    Change font order

--- config.h
+++ config.h
@@ -5,10 +5,10 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hack:size=12:antialias=true:autohint=true";
+static char *font = "Hack Nerd Font:size=12:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
-	"Hack Nerd Font:size=12:antialias=true:autohint=true",
+	"Hack:size=12:antialias=true:autohint=true",
 };
 static int borderpx = 2;
 
